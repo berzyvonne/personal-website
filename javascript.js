13d6f7ca82db92d3bfacbb08b9e9fa87a5900b95
berzyvonne <berzyvonne@gmail.com>
2024-02-14 21:58:18 -0500

    updated About formatting; linked js sheets; cropped images

--- javascript.js
+++ javascript.js
@@ -3,3 +3,11 @@
 // const str= "Hello World"
 // alert(str)
 
+const form = document.querySelector('#form')
+const submitButton = document.querySelector('#submit')
+
+form.addEventListener('submit', (e) => {
+  submitButton.disabled = true
+  e.preventDefault()
+  window.location.href = window.location.origin + '/success.html'
+})
